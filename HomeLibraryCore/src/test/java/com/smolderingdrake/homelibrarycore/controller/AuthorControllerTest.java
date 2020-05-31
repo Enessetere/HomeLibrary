9ba01459886f6d2ca0be67ff188050f596aa8c42
Enessetere <muszalski.sebastian@gmail.com>
2020-05-31 12:13:20 +0200

    AuthorControllerTest completed

--- HomeLibraryCore/src/test/java/com/smolderingdrake/homelibrarycore/controller/AuthorControllerTest.java
+++ HomeLibraryCore/src/test/java/com/smolderingdrake/homelibrarycore/controller/AuthorControllerTest.java
@@ -205,4 +205,74 @@ class AuthorControllerTest {
         assertThat(actualResult).isExactlyInstanceOf(AuthorModel.class);
         assertThat(actualResult).isEqualTo(result);
     }
+
+    @Test
+    void shouldReturnBadRequestCodeDueToValidationInPostMethod() throws Exception {
+        final AuthorModel authorInitials = AuthorModel.builder().firstName("J").lastName("d").build();
+        final AuthorModel authorLastNameInitial = AuthorModel.builder().lastName("D").build();
+        final AuthorModel authorFirstNameInitialAndLastName = AuthorModel.builder().firstName("J").lastName("Doe").build();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/authors")
+                    .content(objectMapper.writeValueAsString(authorInitials))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/authors")
+                    .content(objectMapper.writeValueAsString(authorLastNameInitial))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/authors")
+                    .content(objectMapper.writeValueAsString(authorFirstNameInitialAndLastName))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    void shouldReturnNoContentStatusDueCorrectDataInPutMethod() throws Exception {
+        final AuthorModel author = AuthorModel.builder().idx(1L).firstName("Jane").lastName("Doe").build();
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/authors/1")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(author)))
+                .andExpect(status().isNoContent())
+                .andReturn();
+    }
+
+    @Test
+    void shouldReturnBadRequestStatusDueValidationDataInPutMethod() throws Exception {
+        final AuthorModel author = AuthorModel.builder().idx(1L).firstName("J").lastName("D").build();
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/authors/1")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(author)))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    void shouldReturnNoContentStatusInPatchMethod() throws Exception {
+        final AuthorModel author = AuthorModel.builder().firstName("Jane").build();
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/api/authors/1")
+                    .content(objectMapper.writeValueAsString(author))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andReturn();
+    }
+
+    @Test
+    void shouldReturnNoContentStatusInDeleteMethod() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/authors/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andReturn();
+    }
 }
