34e52de7b497347a9c2b9617eba7b5633888b03d
Enessetere <muszalski.sebastian@gmail.com>
2020-11-04 18:20:02 +0100

    feat(book_view): change books by genre and by author display

--- HomeLibraryCore/src/main/java/com/smolderingdrake/homelibrarycore/bootstrap/Initializer.java
+++ HomeLibraryCore/src/main/java/com/smolderingdrake/homelibrarycore/bootstrap/Initializer.java
@@ -21,11 +21,16 @@ public class Initializer implements ApplicationListener<ContextRefreshedEvent> {
     private static final Author JK_ROWLING = Author.builder().firstName("J. K.").lastName("Rowling").description("Fantasy writer.").build();
     private static final Author GREG_MCLEAN_HALL = Author.builder().firstName("Greg").lastName("McLean Hall").description("science writer").build();
     private static final Author CRAIG_WALLS = Author.builder().firstName("Craig").lastName("Walls").description("science writer").build();
+    private static final Author MARCIN_LIS = Author.builder().firstName("Marcin").lastName("Lis").description("science writer").build();
+    private static final Author DANUTA_MENDRALA = Author.builder().firstName("Danuta").lastName("Mendrala").description("science writer").build();
+    private static final Author MARCIN_SZELIGA = Author.builder().firstName("Marcin").lastName("Szeliga").description("science writer").build();
     private static final Book CHEMISTRY_OF_DEATH = Book.builder().isbn("9788324148455").description("1st part of David Hunter history.").count(1).genre(Genre.CRIMINAL).authors(List.of(SIMON_BECKETT)).title("The Chemistry of Death").build();
     private static final Book WRITTEN_IN_BONE = Book.builder().isbn("9788324151639").description("2nd part of David Hunter history.").count(1).genre(Genre.CRIMINAL).authors(List.of(SIMON_BECKETT)).title("Written in Bone").build();
     private static final Book ADAPTIVE_CODE = Book.builder().isbn("9788328338708").description("Adaptive Code.Nothing more").count(1).genre(Genre.SCIENCE).authors(List.of(GREG_MCLEAN_HALL)).title("Adaptive Code").build();
     private static final Book SPRING_IN_ACTION_4TH = Book.builder().isbn("9788328308497").description("Spring in Action. 4th edition.").count(1).genre(Genre.SCIENCE).authors(List.of(CRAIG_WALLS)).title("Spring in Action. 4th edition").build();
     private static final Book SPRING_IN_ACTION_5TH = Book.builder().isbn("9788328356061").description("Spring in Action. 5th edition.").count(1).genre(Genre.SCIENCE).authors(List.of(CRAIG_WALLS)).title("Spring in Action. 5th edition").build();
+    private static final Book SQL = Book.builder().isbn("9788324694952").description("Practical course").count(1).genre(Genre.SCIENCE).title("SQL").authors(List.of(DANUTA_MENDRALA, MARCIN_SZELIGA)).build();
+    private static final Book CSharp = Book.builder().isbn("9788328314566").description("Practical course").count(1).genre(Genre.SCIENCE).title("C#").authors(List.of(MARCIN_LIS)).build();
 
     private final AuthorService authorService;
     private final BookService bookService;
@@ -44,5 +49,7 @@ public class Initializer implements ApplicationListener<ContextRefreshedEvent> {
         bookService.createBook(ADAPTIVE_CODE);
         bookService.createBook(SPRING_IN_ACTION_4TH);
         bookService.createBook(SPRING_IN_ACTION_5TH);
+        bookService.createBook(SQL);
+        bookService.createBook(CSharp);
     }
 }
