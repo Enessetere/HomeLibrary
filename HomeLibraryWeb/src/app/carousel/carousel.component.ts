186ad1775a5ac0187a983c05ad89e51b7ea5945f
Enessetere <muszalski.sebastian@gmail.com>
2020-11-06 11:44:49 +0100

    styles(carousel): change display of carousel item

--- HomeLibraryWeb/src/app/carousel/carousel.component.ts
+++ HomeLibraryWeb/src/app/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Book} from '../book';
 
 @Component({
   selector: 'app-carousel',
@@ -7,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  @Input() books;
+  @Input() books: Book[];
 
   currentSlide = 0;
 
@@ -19,10 +20,12 @@ export class CarouselComponent implements OnInit {
   onPreviousClick() {
     const prev = this.currentSlide - 1;
     this.currentSlide = prev < 0 ? this.books.length - 1 : prev;
+    document.getElementById('previous').blur();
   }
 
   onNextClick() {
     const next = this.currentSlide + 1;
     this.currentSlide = next === this.books.length ? 0 : next;
+    document.getElementById('next').blur();
   }
 }
